e30c163ee2944710112e0c2f913ad315199d7c7a
Florian J. Breunig <Florian.J.Breunig@my-flow.com>
2016-01-07 23:00:53 +0100

    Extend copyright notice

--- src/main/java/com/moneydance/modules/features/paypalimporter/package-info.java
+++ src/main/java/com/moneydance/modules/features/paypalimporter/package-info.java
@@ -1,5 +1,5 @@
 // PayPal Importer for Moneydance - http://my-flow.github.io/paypalimporter/
-// Copyright (C) 2013-2015 Florian J. Breunig. All rights reserved.
+// Copyright (C) 2013-2016 Florian J. Breunig. All rights reserved.
 
 /**
  * Contains the main class.
