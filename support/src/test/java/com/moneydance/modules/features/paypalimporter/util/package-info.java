028e0a43e563ed75b4dd64ddc90bbca57a5cb333
Florian J. Breunig <Florian.J.Breunig@my-flow.com>
2018-07-14 21:24:46 +0200

    Update Gradle
    Fix Checkstyle-related deprecation warning issued by Gradle 4.10.3.

---
+++ support/src/test/java/com/moneydance/modules/features/paypalimporter/util/package-info.java
@@ -0,0 +1,7 @@
+// PayPal Importer for Moneydance - http://my-flow.github.io/paypalimporter/
+// Copyright (C) 2013-2018 Florian J. Breunig. All rights reserved.
+
+/**
+ * @author Florian J. Breunig
+ */
+package com.moneydance.modules.features.paypalimporter.util;
